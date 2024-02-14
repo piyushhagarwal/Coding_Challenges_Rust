5db1e470e7db4fbeec07f4b739aec9d77d2c8c77
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-02-14 13:02:29 +0530

    Refactor ccwc/src/main.rs to improve code readability and add functionality for further steps

--- ccwc/src/main.rs
+++ ccwc/src/main.rs
@@ -1,6 +1,23 @@
-use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::env; // Module to get the command line arguments
+use std::fs::File; // Module to work with files
+use std::io::Read; // Module to read from files
+
+fn count_bytes(contents: &str) -> usize {
+    // Give the number of bytes in the file
+    contents.len()
+}
+
+fn count_words(contents: &str) -> usize {
+    // Give the number of words in the file
+    let total_words = contents.split_whitespace().count();
+    total_words
+}
+
+fn count_lines(contents: &str) -> usize {
+    // Give the number of lines in the file
+    let total_lines = contents.lines().count();
+    total_lines
+}
 
 fn main() {
     let args : Vec<String> = env::args().collect(); // collect the command line arguments into a vector
@@ -14,25 +31,59 @@ fn main() {
     // Print the vector
     // println!("{:?}", args); 
 
-    if args.len() != 3 {
+    // Length of the vector
+    let args_len = args.len() - 1; // subtract 1 to exclude the path of the program
+
+    if args_len == 2{
+        // Get the method from the arguments
+        let method = &args[1];
+
+        // Get the file name from the arguments
+        let file_name = &args[2];
+
+        // Open the file
+        let mut file = File::open(file_name).expect("File not found");
+
+        // Read the file
+        // Read from the file
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Failed to read file");
+
+        if method == "-c" {
+            println!("  {} {}", count_bytes(&contents), file_name);
+        }
+        else if method == "-w" {
+            println!("  {} {}", count_words(&contents), file_name);
+        } 
+        else if method == "-l" {
+            println!("  {} {}", count_lines(&contents), file_name);
+        }
+        else {
+            eprintln!("Usage: ccwc <method> <file_name>");
+            std::process::exit(1);
+        }
+
+    }
+
+    else if args_len == 1 {
+        // Get the file name from the arguments
+        let file_name = &args[1];
+
+        // Open the file
+        let mut file = File::open(file_name).expect("File not found");
+
+        // Read the file
+        // Read from the file
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Failed to read file");
+
+        // Give the number of lines, words, and bytes in the file
+        println!("  {} {} {} {}", count_lines(&contents), count_words(&contents), count_bytes(&contents), file_name);
+    }
+
+    else{
         eprintln!("Usage: ccwc <method> <file_name>");
         std::process::exit(1);
     }
 
-    // Get the method from the arguments
-    let method = &args[1];
-
-    // Get the file name from the arguments
-    let file_name = &args[2];
-
-    // Open the file
-    let mut file = File::open(file_name).expect("File not found");
-
-    // Read the file
-    // Read from the file
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Failed to read file");
-
-    // Give the number of bytes in the file
-    println!("  {} {}", contents.len(), file_name);
 }
