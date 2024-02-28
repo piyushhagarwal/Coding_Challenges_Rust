80014455c464be06827a9591638dc7becdf58d0c
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-02-28 15:22:52 +0530

    Implementation of generate_frequency_table. Completed step 1 of challenge

---
+++ compression_tool/src/frequency_table.rs
@@ -0,0 +1,45 @@
+use std::fs::File; // Module to work with files
+use std::io::Read; // Module to read from files
+use std::collections::HashMap; // Module to work with hashmaps
+
+fn generate_frequency_table(file_name : &str) -> HashMap<char, i32>{
+    // Open the file
+    let mut file = File::open(file_name).expect("Problem opening the file");
+
+    // Read from the file
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Failed to read file");
+
+    // Create a hashmap to store the frequency of each character
+    let mut frequency_map = HashMap::new();
+
+    // Iterate over the characters of the file
+    for c in contents.chars() {
+        // If the character is not in the hashmap, add it with a frequency of 1
+        if !frequency_map.contains_key(&c) {
+            frequency_map.insert(c, 1);
+        } else {
+            // If the character is already in the hashmap, increment its frequency
+            let count = frequency_map.get_mut(&c).unwrap();
+            *count += 1;
+        }
+    }
+
+    // Return the frequency_map
+    frequency_map
+}
+
+// Test cases
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_frequency_table() {
+        let file_name = "test_file.txt";
+        let frequency_map = generate_frequency_table(file_name);
+        assert_eq!(frequency_map.get(&'X'), Some(&333));
+        assert_eq!(frequency_map.get(&'t'), Some(&223000));
+    }
+}
---
+++ compression_tool/src/lib.rs
@@ -0,0 +1,1 @@
+mod frequency_table;
