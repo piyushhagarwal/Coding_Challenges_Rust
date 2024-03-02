743308a934b2f64b9bb5d1c00f6e038f15fc9103
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-03-02 16:10:47 +0530

    step 4 add header file

--- compression_tool/src/lib.rs
+++ compression_tool/src/lib.rs
@@ -1,6 +1,7 @@
 mod frequency_table;
 mod huffman_binary_tree;
 mod prefix_table;
+mod header_file;
 
 pub use frequency_table::generate_frequency_table;
 pub use huffman_binary_tree::generate_huffman_tree;
